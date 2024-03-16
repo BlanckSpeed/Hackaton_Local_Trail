a8d6c69ea549e59be0b00700cb5e84e9b8fc9e9a
Rodrigo Lendinez Ruiz <BlanckSpeed>
2024-03-16 14:46:59 +0100

    feat: Android frame add with code for figma

---
+++ _Android_frame_2/java/exportkit/figma/frame_2_activity.java
@@ -0,0 +1,155 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		frame_2
+	 *	@date 		Saturday 16th of March 2024 12:16:20 PM
+	 *	@title 		App
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.content.Intent;
+import android.widget.TextView;
+
+public class frame_2_activity extends Activity {
+
+	
+	private View _bg__frame_2_ek2;
+	private View _bg__customer;
+	private ImageView customer_ek4;
+	private View _bg__vector;
+	private ImageView vector_ek4;
+	private View _bg__vector_1;
+	private ImageView vector_ek9;
+	private View _bg__compass_north;
+	private View ellipse_28;
+	private ImageView compass_north_ek4;
+	private View _bg__component_1;
+	private View ellipse_29;
+	private View _bg__vector_ek11;
+	private View _bg__component_2;
+	private View ellipse_30;
+	private View _bg__vector_ek13;
+	private ImageView vector_ek14;
+	private View _ellipse_41;
+	private TextView _2;
+	private View _bg__vector_2;
+	private ImageView vector_ek19;
+	private ImageView vector_ek20;
+	private View _bg__vector_3;
+	private ImageView vector_ek25;
+	private View rectangle_32;
+	private View rectangle_33;
+	private View rectangle_34;
+	private View rectangle_31;
+	private View _bg__close;
+	private ImageView close_ek4;
+	private View _bg__vector_4;
+	private ImageView vector_ek30;
+	private View _bg__vector_5;
+	private ImageView vector_ek35;
+	private View _bg__vector_6;
+	private ImageView vector_ek40;
+	private View _bg__property_1_group;
+	private ImageView vector_ek41;
+	private ImageView vector_ek42;
+	private View _bg__property_1_group_1;
+	private ImageView vector_ek43;
+	private ImageView vector_ek44;
+	private View _bg__property_1_star_rate;
+	private View _bg__group_ek1;
+	private ImageView vector_ek45;
+	private ImageView vector_ek46;
+	private View _bg__vector_7;
+	private ImageView vector_ek51;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.frame_2);
+
+		
+		_bg__frame_2_ek2 = (View) findViewById(R.id._bg__frame_2_ek2);
+		_bg__customer = (View) findViewById(R.id._bg__customer);
+		customer_ek4 = (ImageView) findViewById(R.id.customer_ek4);
+		_bg__vector = (View) findViewById(R.id._bg__vector);
+		vector_ek4 = (ImageView) findViewById(R.id.vector_ek4);
+		_bg__vector_1 = (View) findViewById(R.id._bg__vector_1);
+		vector_ek9 = (ImageView) findViewById(R.id.vector_ek9);
+		_bg__compass_north = (View) findViewById(R.id._bg__compass_north);
+		ellipse_28 = (View) findViewById(R.id.ellipse_28);
+		compass_north_ek4 = (ImageView) findViewById(R.id.compass_north_ek4);
+		_bg__component_1 = (View) findViewById(R.id._bg__component_1);
+		ellipse_29 = (View) findViewById(R.id.ellipse_29);
+		_bg__vector_ek11 = (View) findViewById(R.id._bg__vector_ek11);
+		_bg__component_2 = (View) findViewById(R.id._bg__component_2);
+		ellipse_30 = (View) findViewById(R.id.ellipse_30);
+		_bg__vector_ek13 = (View) findViewById(R.id._bg__vector_ek13);
+		vector_ek14 = (ImageView) findViewById(R.id.vector_ek14);
+		_ellipse_41 = (View) findViewById(R.id._ellipse_41);
+		_2 = (TextView) findViewById(R.id._2);
+		_bg__vector_2 = (View) findViewById(R.id._bg__vector_2);
+		vector_ek19 = (ImageView) findViewById(R.id.vector_ek19);
+		vector_ek20 = (ImageView) findViewById(R.id.vector_ek20);
+		_bg__vector_3 = (View) findViewById(R.id._bg__vector_3);
+		vector_ek25 = (ImageView) findViewById(R.id.vector_ek25);
+		rectangle_32 = (View) findViewById(R.id.rectangle_32);
+		rectangle_33 = (View) findViewById(R.id.rectangle_33);
+		rectangle_34 = (View) findViewById(R.id.rectangle_34);
+		rectangle_31 = (View) findViewById(R.id.rectangle_31);
+		_bg__close = (View) findViewById(R.id._bg__close);
+		close_ek4 = (ImageView) findViewById(R.id.close_ek4);
+		_bg__vector_4 = (View) findViewById(R.id._bg__vector_4);
+		vector_ek30 = (ImageView) findViewById(R.id.vector_ek30);
+		_bg__vector_5 = (View) findViewById(R.id._bg__vector_5);
+		vector_ek35 = (ImageView) findViewById(R.id.vector_ek35);
+		_bg__vector_6 = (View) findViewById(R.id._bg__vector_6);
+		vector_ek40 = (ImageView) findViewById(R.id.vector_ek40);
+		_bg__property_1_group = (View) findViewById(R.id._bg__property_1_group);
+		vector_ek41 = (ImageView) findViewById(R.id.vector_ek41);
+		vector_ek42 = (ImageView) findViewById(R.id.vector_ek42);
+		_bg__property_1_group_1 = (View) findViewById(R.id._bg__property_1_group_1);
+		vector_ek43 = (ImageView) findViewById(R.id.vector_ek43);
+		vector_ek44 = (ImageView) findViewById(R.id.vector_ek44);
+		_bg__property_1_star_rate = (View) findViewById(R.id._bg__property_1_star_rate);
+		_bg__group_ek1 = (View) findViewById(R.id._bg__group_ek1);
+		vector_ek45 = (ImageView) findViewById(R.id.vector_ek45);
+		vector_ek46 = (ImageView) findViewById(R.id.vector_ek46);
+		_bg__vector_7 = (View) findViewById(R.id._bg__vector_7);
+		vector_ek51 = (ImageView) findViewById(R.id.vector_ek51);
+	
+		
+		_ellipse_41.setOnClickListener(new View.OnClickListener() {
+		
+			public void onClick(View v) {
+				
+				Intent nextScreen = new Intent(getApplicationContext(), iphone_14___15_pro___15_activity.class);
+				startActivity(nextScreen);
+			
+		
+			}
+		});
+		
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
